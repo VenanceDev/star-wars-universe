2bfdaf203671a8c9a8319b693defb187719f7194
Venance Konan <venancekonan5@gmail.com>
2020-09-12 12:22:09 +0200

    Updates

---
+++ precache-manifest.c4f41760e0e98508253a02e2e16ee7ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291a92e2a6a7d70bacabd2bd4b204cd6",
+    "url": "/star-wars-universe/index.html"
+  },
+  {
+    "revision": "6f7519a4d03032265294",
+    "url": "/star-wars-universe/static/css/main.d220d8ee.chunk.css"
+  },
+  {
+    "revision": "1618b05a55c2f8bd9be7",
+    "url": "/star-wars-universe/static/js/2.ade7b711.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/star-wars-universe/static/js/2.ade7b711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7519a4d03032265294",
+    "url": "/star-wars-universe/static/js/main.784d649c.chunk.js"
+  },
+  {
+    "revision": "2bef85bece5ae7e8237c",
+    "url": "/star-wars-universe/static/js/runtime-main.7ba41f31.js"
+  }
+]);
